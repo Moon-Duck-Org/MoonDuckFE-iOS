ccb7a0e56ec1a630ddf7f5328a97f6cde5ebd339
SuniDev <sun-918@naver.com>
2024-06-03 21:14:59 +0900

    test : API 테스트 코드 추가

--- MoonDuck/MoonDuck/Sources/Common/Networking/Service/ReviewService.swift
+++ MoonDuck/MoonDuck/Sources/Common/Networking/Service/ReviewService.swift
@@ -10,6 +10,8 @@ import Alamofire
 class ReviewService {
     
     func getReview(request: GetReviewRequest, completion: @escaping (_ succeed: [Review]?, _ failed: Error?) -> Void) {
+        completion([], nil)
+        
         API.session.request(MoonDuckAPI.getReview(request))
             .responseDecodable(of: [ReviewResponse].self) { response in
                 switch response.result {
@@ -22,6 +24,8 @@ class ReviewService {
     }
     
     func reviewAll(request: ReviewAllRequest, completion: @escaping (_ succeed: [Review]?, _ failed: Error?) -> Void) {
+        completion([], nil)
+        
         API.session.request(MoonDuckAPI.reviewAll(request))
             .responseDecodable(of: [ReviewResponse].self) { response in
                 switch response.result {
@@ -34,6 +38,8 @@ class ReviewService {
     }
     
     func putReview(request: PutReviewRequest, completion: @escaping (_ succeed: Review?, _ failed: Error?) -> Void) {
+        completion(Review(id: 0, title: "타이틀", created: "날짜", nickname: "포덕이", category: .movie, content: "내용", imageUrlList: [], rating: 5), nil)
+        
         API.session.request(MoonDuckAPI.putReview(request))
             .responseDecodable(of: ReviewResponse.self) { response in
                 switch response.result {
@@ -46,6 +52,8 @@ class ReviewService {
     }
     
     func postReview(request: PostReviewRequest, completion: @escaping (_ succeed: Review?, _ failed: Error?) -> Void) {
+        completion(Review(id: 0, title: "타이틀", created: "날짜", nickname: "포덕이", category: .movie, content: "내용", imageUrlList: [], rating: 5), nil)
+        
         API.session.request(MoonDuckAPI.postReview(request))
             .responseDecodable(of: ReviewResponse.self) { response in
                 switch response.result {
@@ -58,6 +66,8 @@ class ReviewService {
     }
     
     func deleteReview(request: DeleteReviewRequest, completion: @escaping (_ succeed: Bool?, _ failed: Error?) -> Void) {
+        completion(true, nil)
+        
         API.session.request(MoonDuckAPI.deleteReview(request))
             .responseDecodable { (response: AFDataResponse<Bool>) in
                 switch response.result {
@@ -70,6 +80,8 @@ class ReviewService {
     }
     
     func reviewDetail(request: ReviewDetailRequest, completion: @escaping (_ succeed: Review?, _ failed: Error?) -> Void) {
+        completion(Review(id: 0, title: "타이틀", created: "날짜", nickname: "포덕이", category: .movie, content: "내용", imageUrlList: [], rating: 5), nil)
+        
         API.session.request(MoonDuckAPI.reviewDetail(request))
             .responseDecodable(of: ReviewResponse.self) { response in
                 switch response.result {
--- MoonDuck/MoonDuck/Sources/Common/Networking/Service/UserService.swift
+++ MoonDuck/MoonDuck/Sources/Common/Networking/Service/UserService.swift
@@ -7,11 +7,11 @@
 
 import Alamofire
 
+// FIXME: - TEST CODE API
 class UserService {
     func user(request: UserRequest, completion: @escaping (_ succeed: User?, _ failed: Error?) -> Void) {
+        completion(User(id: 0, deviceId: "test", nickname: "포덕이"), nil)
         
-        // FIXME: - TEST CODE
-//        API.session.request(MoonDuckAPI.user(UserRequest(deviceId: "test")))
         API.session.request(MoonDuckAPI.user(request))
             .responseDecodable { (response: AFDataResponse<UserResponse>) in
                 switch response.result {
@@ -24,6 +24,8 @@ class UserService {
     }
     
     func login(request: UserLoginRequest, completion: @escaping (_ succeed: Bool?, _ failed: Error?) -> Void) {
+        completion(true, nil)
+        
         API.session.request(MoonDuckAPI.userLogin(request))
             .responseDecodable { (response: AFDataResponse<Bool>) in
                 switch response.result {
@@ -36,6 +38,8 @@ class UserService {
     }
     
     func nickname(request: UserNicknameRequest, completion: @escaping (_ succeed: User?, _ failed: Error?) -> Void) {
+        completion(User(id: 0, deviceId: "test", nickname: "포덕이"), nil)
+        
         API.session.request(MoonDuckAPI.userNickname(request))
             .responseDecodable { (response: AFDataResponse<UserResponse>) in
                 switch response.result {
