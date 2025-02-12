d5f64b309a0875d992b1e9f26e156184950841f0
SuniDev <sun-918@naver.com>
2025-02-12 23:33:59 +0900

    Remove Pod

--- MoonDuck/Pods/Target Support Files/Pods-MoonDuck-MoonDuckUITests/Pods-MoonDuck-MoonDuckUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MoonDuck_MoonDuckUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MoonDuck_MoonDuckUITests
-@end
--- MoonDuck/Pods/Target Support Files/Pods-MoonDuck/Pods-MoonDuck-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MoonDuck : NSObject
-@end
-@implementation PodsDummy_Pods_MoonDuck
-@end
